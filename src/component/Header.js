206a6970a7e1446f9c8794f1095bb53dce35eedb
twentyFourDesign <haiderhassane@gmail.com>
2023-03-29 17:01:52 +0500

    logo to link

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../image/Jollof by Jara 1.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header-container">
       <div className="image-container">
-        <img className="header-image" src={logo} alt="logo" />
+        <Link to="/">
+          <img className="header-image" src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="menu-container">
         <h1 className="menu">MENU</h1>
